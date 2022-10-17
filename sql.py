e5abfb6484ac8c3c80b9c1018d71fbe9fde02229
Ram Sankar <RamSankarTheDeveloper@gmail.com>
2022-10-17 08:16:11 +0530

    sql test file updated with try_except.

--- sql.py
+++ sql.py
@@ -1,12 +1,17 @@
 #file used to reset the table in development phase
 import sqlite3
 
-#c.execute('''CREATE table items_prices(items TEXT, price FLOAT)''')  #create table
-
 conn = sqlite3.connect('storage_data.db')
 c=conn.cursor()
+try:
+    c.execute('''CREATE table items_prices(items TEXT, price FLOAT)''')  #create table
+except:
+    pass
+
+
 
 #c.execute('''drop table items_prices''')  #delete table
+
 c.execute('''SELECT * from items_prices''')  #display table
 print(c.fetchall())  #display table
 
